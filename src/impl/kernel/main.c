979da1a67d769fc0abb876d1bd74701055c49ad6
John Young <polomint77@hotmail.com>
2021-03-13 14:06:48 +0000

    Fixing bits

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,8 +1,8 @@
 #include "sys_globals.h"
 #include "print.h"
 #include "frame.h"
-//#include "console.h"
-#include "input.h"
+#include "console.h"
+
 
 void kernel_main() {
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLUE);
@@ -12,10 +12,10 @@ void kernel_main() {
     draw_frame(0, 1, 79, 18, PRINT_COLOR_BLACK, PRINT_COLOR_MAGENTA, " Code Editor [untitled.asm]");
     draw_frame(0, 20, 79, 4, PRINT_COLOR_WHITE, PRINT_COLOR_GREEN, " Console ");
     draw_frame(5, 5, 5, 5, PRINT_COLOR_WHITE, PRINT_COLOR_GREEN, " This is a test frame ");
-    kb_init();
+    //kb_init();
     while (1)
     {
-        keyboard_handler();
+        //keyboard_handler();
     }
     //Console_Run();
 }
